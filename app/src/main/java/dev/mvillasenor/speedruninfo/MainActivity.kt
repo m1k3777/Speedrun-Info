f2977a43928a57df2e3b34422760af542dd96fc5
= <miguel.villasenor@wizeline.com>
2019-06-28 13:56:49 -0500

    Initial Commit

--- app/src/main/java/dev/mvillasenor/speedruninfo/MainActivity.kt
+++ app/src/main/java/dev/mvillasenor/speedruninfo/MainActivity.kt
@@ -1,12 +1,24 @@
 package dev.mvillasenor.speedruninfo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
+    @FlowPreview
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
