356e752d2707fc8096d85f1760ec27ac4f41f31c
miguel <miguel.villasenor@outlook.com>
2020-03-02 11:47:55 -0600

    Reformatting

---
+++ app/src/main/java/dev/mvillasenor/speedruninfo/ui/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package dev.mvillasenor.speedruninfo.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.mvillasenor.speedruninfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
